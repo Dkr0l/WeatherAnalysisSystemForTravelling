8b1e3dd3ad7cf218b6f73ed25b2a65436cec1566
Mateusz <matimatik97@gmail.com>
2020-10-31 14:10:50 +0100

    Baza danych Firebase

--- app/src/main/java/krolkozak/project/app/GlownaAktywnosc.java
+++ app/src/main/java/krolkozak/project/app/GlownaAktywnosc.java
@@ -16,8 +16,12 @@ import androidx.annotation.RequiresApi;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 import org.osmdroid.config.Configuration;
 
+import java.time.OffsetDateTime;
 import java.util.ArrayList;
 
 @RequiresApi(api = Build.VERSION_CODES.O)
@@ -81,6 +85,19 @@ public class GlownaAktywnosc extends Activity {
 
         znajdzTrasePrzycisk.setText("STWÓRZ TRASĘ");
         znajdzTrasePrzycisk.setEnabled(false);
+
+        // TESTOWANIE BAZY DANYCH -------------------------------------------------
+
+        // Odnośnik do bazy danych
+        FirebaseDatabase bazaDanychRef = FirebaseDatabase.getInstance();
+        // Odnośnik do tabeli uzytkownicy
+        DatabaseReference uzytkownicyRef = bazaDanychRef.getReference("uzytkownicy");
+
+        // Utworzenie nowego użytkownika i wprowadzenie go do tabeli uzytkownicy
+        Uzytkownik uzytkownik1 = new Uzytkownik("login2", "haslo2", "email2", String.valueOf(OffsetDateTime.now()));
+        uzytkownicyRef.child("email2").setValue(uzytkownik1);
+
+        Log.i(nazwaApki, "Wprowadzono nowego użytkownika do tabeli: " + uzytkownik1.pobierzPelneDane());
     }
 
     // metoda prosząca użytkownika o zezwolenia na uprawnienia
